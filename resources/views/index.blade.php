9a01c9258e7641e16a42d73602c3e78b4ce356c8
reksageni123 <alifpandoyo40@gmail.com>
2024-05-18 15:30:13 +0700

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alif Portfolio /></title>
-    @vite('resources/css/app.css')
+    
+    <script src="https://cdn.tailwindcss.com"></script>
+
   
 </head>
     <body class="bg-slate-600">
